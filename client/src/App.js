43072d363840cc1cee002cb80cd8b8e91a856187
libre-maiden <maiden@disroot.org>
2022-06-21 00:05:45 +0300

    Added routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,25 @@
 import React, {Component} from 'react';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
 import './App.css';
 import Menu from './components/Menu';
-import Post from './components/Post'
+import PostList from './components/PostList'
+import About from './components/About';
 
 class App extends Component {
   render () {
     return (
       <div className="App">
         <Menu />
-        <Post />
+        <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<PostList />}></Route>
+          <Route path="/about" element={<About />}></Route>
+        </Routes>
+        </BrowserRouter>
       </div>
     );
   }
